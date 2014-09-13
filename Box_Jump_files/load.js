84523b1cf394a7cb4630ab42972e38d6171646f9
dungnv53 <vandung_17@yahoo.com>
2014-09-13 23:49:19 +0700

    Update bg, box color

--- Box_Jump_files/load.js
+++ Box_Jump_files/load.js
@@ -14,6 +14,7 @@ Game.Load = function (game) {
 Game.Load.prototype = {
 	preload: function () {
 	    game.stage.backgroundColor = '#669999';
+	    // game.load.image("background",'./images/bg2.jpg');
 	    label1 = game.add.text(Math.floor(w/2), Math.floor(h/2)-20, 'Box Jump', { font: '30px Arial', fill: '#fff' });
 	    label2 = game.add.text(Math.floor(w/2)+0.5, Math.floor(h/2)+20+0.5, 'loading...', { font: '16px Arial', fill: '#fff' });
 		label1.anchor.setTo(0.5, 0.5);
@@ -23,7 +24,8 @@ Game.Load.prototype = {
 		game.load.image('player', 'images/light_blue_20.png');
 		game.load.image('line', 'images/line.png');
 		// game.load.image('cube', 'images/cube.png');
-		game.load.image('cube', 'images/light_orange_20.png');
+		game.load.image('cube', 'images/orange_20.png');
+		// game.load.image('cube', 'images/light_blue_20.png');
 		game.load.image('pixel', 'images/pixel.png');
 		game.load.audio('hit', 'sounds/hit.wav');
 		game.load.audio('jump', 'sounds/jump.wav');
